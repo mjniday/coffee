2fe1d232d9d4ce6dd35edc30dae4f56647964265
mjniday <michael.niday@gmail.com>
2016-12-17 20:29:18 -0500

    Added Calculator component

--- src/BrewCalculator.js
+++ src/BrewCalculator.js
@@ -1,10 +1,40 @@
 import React, { Component } from 'react';
 
 class BrewCalculator extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      coffeeWeight: 42,
+      waterWeight: 714,
+      boxSizing: 'borderBox'
+    };
+
+    this.calculateCoffee = this.calculateCoffee.bind(this);
+    this.calculateWater = this.calculateWater.bind(this);
+  }
+  calculateCoffee(event) {
+    this.setState({ 
+      coffeeWeight: Math.round(event.target.value / 17 * 100) / 100,
+      waterWeight: event.target.value
+    })
+  }
+  calculateWater(event) {
+    this.setState({ 
+      coffeeWeight: event.target.value,
+      waterWeight: event.target.value * 17
+    })
+  }
   render() {
     return (
       <div>
-        I'm a calculator
+        <div className="input-container w-100">
+          <input className="input f3 w-75" onChange={this.calculateWater} value={this.state.coffeeWeight} placeholder="grams"></input>
+          <span className="dib w-25">g coffee</span>
+        </div>
+        <div className="input-container w-100">
+          <input className="input f3 w-75" onChange={this.calculateCoffee} value={this.state.waterWeight} placeholder="grams"></input>
+          <span className="dib w-25">g water</span>
+        </div>
       </div>
     );
   }
